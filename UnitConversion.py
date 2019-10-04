cc3fb954541e7916d99b9a1e8efc96cfb194e2ae
Sathish Sandu <Sathish.Sandu@ablevets.com>
2019-10-04 17:01:59 -0400

    update 2

--- UnitConversion.py
+++ UnitConversion.py
@@ -2,23 +2,32 @@
 temperatures_uom_list = ["Kelvin", "Celsius", "Fahrenheit", "Rankine"]
 volumes_uom_list = ["liters", "tablespoons", "cubic-inches", "cups", "cubic-feet", "gallons"]
 
-
-def inputs(temperature_uom_type, volume_uom_type):
-    print("The inputs are: {0}, {1}".format(temperature_uom_type, volume_uom_type))
-    # print("temperature type is: {1}".format(volume_uom_type))
-    if(temperature_uom_type) in temperatures_uom_list:
-        print("Valid Temperature Type")
+def validate_uom(input_uom):
+    isValid = False
+    print("input_uom value is: ", format(input_uom))
+    # check if input_uom is in temperatures_uom_list
+    if(input_uom) in temperatures_uom_list:
+        isValid = True
+    elif(input_uom) in volumes_uom_list:
+        isValid = True
     else:
-        print("Invalid Temperature Type")
-    if(volume_uom_type) in volumes_uom_list:
-        print("Valid Volume Type")
-    else:
-        print("Invalid Volume Type")
-    
+        print("input_uom value entered is invalid")
+    return isValid 
 
-temperature_uom_type = input("Please enter temperature type: ")
-volume_uom_type = input("Please enter volume type: ")
-# greeting = input("Enter Greeting: ")
-inputs(temperature_uom_type, volume_uom_type)
+def inputs(input_numerical_value, input_uom, target_uom, student_numeric_response):
+    print("The inputs are: {0}, {1}, {2}, {3}".format(input_numerical_value, input_uom, target_uom, student_numeric_response))
+    isInput_UOM_Valid = validate_uom(input_uom)
+    print(isInput_UOM_Valid)
 
-# greet("sathish", "good morning")
+
+input_numerical_value = input("Please enter input_numerical_value: ")
+input_uom = input("Please enter input_uom: ")
+target_uom = input("Please enter target_uom: ")
+student_numeric_response = input("Please enter student_numeric_response: ")
+
+inputs(input_numerical_value, input_uom, target_uom, student_numeric_response)
+
+
+
+#1.	The teacher must be able to provide an input numerical value, an input unit of measure, a target unit of measure, and a student’s numeric response.
+#2.	The system indicates that the response is correct, incorrect, or invalid. To be considered correct, the student’s response must match an authoritative answer after both the student’s response and authoritative answer are rounded to the tenths place. 
