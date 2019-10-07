2b2a90f5103f1abc4731c60433dc069d2c64dceb
Sathish Sandu <Sathish.Sandu@ablevets.com>
2019-10-07 16:02:37 -0400

    Cleaned up code

--- UnitConversion.py
+++ UnitConversion.py
@@ -1,4 +1,4 @@
-
+import sys
 
 temperatures_uom_list = ["Kelvin", "Celsius", "Fahrenheit", "Rankine"]
 volumes_uom_list = ["liters", "tablespoons", "cubic-inches", "cups", "cubic-feet", "gallons"]
@@ -6,47 +6,43 @@ volumes_uom_list = ["liters", "tablespoons", "cubic-inches", "cups", "cubic-feet
 # First validate input_uom value - is it in the specified list of temperature types or volume types?
 def validate_input_uom(input_uom):
     isValid = False
-    
+
     if(input_uom) in temperatures_uom_list:
         isValid = True
     elif(input_uom) in volumes_uom_list:
         isValid = True
-    else:
-        print("input_uom value entered is invalid")
+
     return isValid 
 
 # validate if the target_uom is valid for the specified input_uom
 def validate_target_uom(input_uom, target_uom):
     isValid = False
-    print("input_uom value is: ", format(input_uom))
-    print("target_uom value is: ", format(target_uom))
-    # check if input_uom is in temperatures_uom_list
+
     if(input_uom) in temperatures_uom_list:
         if(target_uom) in temperatures_uom_list:
             isValid = True
     elif(input_uom) in volumes_uom_list:
         if(target_uom) in volumes_uom_list:
             isValid = True
-#    else:
-#       print("target_uom value entered is invalid")
+
     return isValid 
 
 # conversion function to convert input_numerical_value from input_uom to target_uom
 def convertInputUOM_to_TargetUOM(input_numerical_value, input_uom, target_uom):
-    convertedOutput = 0
+    convertedOutput = 0.0
+
     if(input_uom) in temperatures_uom_list:
-        print("Invoking convertTemperature function")
         convertedOutput = convertTemperature(input_numerical_value, input_uom, target_uom)
-        return convertedOutput
     if(input_uom) in volumes_uom_list:
-        print("Invoking convertVolumes function")
         convertedOutput = convertVolume(input_numerical_value, input_uom, target_uom)
-        return convertedOutput
+        
+    return convertedOutput
 
 def convertTemperature(input_numerical_value, input_uom, target_uom):
     outputTemp = 0.0
     # convert input_uom to 'Kelvin' temperature UOM.
     k = toK[input_uom](float(input_numerical_value))
+
     # now calculate all the temperature values in temperatures_uom_list.
     celsiusValue = k - 273.15
     fahrenheitValue = k * 1.8 - 459.67
@@ -63,13 +59,12 @@ def convertTemperature(input_numerical_value, input_uom, target_uom):
         outputTemp = rankineValue
 
     # round outputTemp to the tenths decimal place
-    print("outputTemp before rounding is:", outputTemp)
     outputTemp = round(outputTemp, 1)
     return outputTemp
 
 def convertVolume(input_numerical_value, input_uom, target_uom):
-    outputVolume = 0
-    # convert input_uom to 'Kelvin' temperature UOM.
+    outputVolume = 0.0
+    # convert input_uom to 'tablespoons' volume UOM.
     t = toT[input_uom](float(input_numerical_value))
 
     # now calculate all the volume values in volumes_uom_list.
@@ -80,7 +75,6 @@ def convertVolume(input_numerical_value, input_uom, target_uom):
     cubicFeetValue = t * 0.00052219
     tablespoonsValue = t
 
-    # volumes_uom_list = ["liters", "tablespoons", "cubic-inches", "cups", "cubic-feet", "gallons"]
     if target_uom == "liters":
         outputVolume = litersValue
     elif target_uom == "tablespoons":
@@ -95,7 +89,6 @@ def convertVolume(input_numerical_value, input_uom, target_uom):
         outputVolume = gallonsValue
 
     # round outputVolume to the tenths decimal place
-    print("outputVolume before rounding is:", outputVolume)
     outputVolume = round(outputVolume, 1)
     return outputVolume
 
@@ -114,34 +107,53 @@ toT = { 'liters': (lambda l: l * 67.628),
         'cubicFeet': (lambda cf: cf * 1915.01),
         'tablespoons': (lambda t: t) }
 
-
-
-def inputs(input_numerical_value, input_uom, target_uom, student_numeric_response):
+# This is the main function that takes the input values,  and invokes functions to validate input_uom/target_uom and to convert input_numerical_value to the corresponding value in target_uom
+def processInputs(input_numerical_value, input_uom, target_uom, student_numeric_response):
     programConversionOutput = 0
     # round student_numeric_response to the tenth decimal place
     student_numeric_response = round(float(student_numeric_response), 1)
-    print("The inputs are: {0}, {1}, {2}, {3}".format(input_numerical_value, input_uom, target_uom, student_numeric_response))
+
     # validate input_uom
     isInput_UOM_Valid = validate_input_uom(input_uom)
-    print("is input_uom value valid?:", format(isInput_UOM_Valid))
+
     # validate target_uom only if input_uom is valid
     if(isInput_UOM_Valid):
         isTarget_UOM_Valid = validate_target_uom(input_uom, target_uom)
-        print("is target_uom value valid?:", format(isTarget_UOM_Valid))
         # invoke convert function only if input_uom and target_uom are valid
         if(isTarget_UOM_Valid):
             programConversionOutput = convertInputUOM_to_TargetUOM(input_numerical_value, input_uom, target_uom)
-            print("Program conversion output is", format(programConversionOutput))
-            if programConversionOutput == student_numeric_response:
-                print("Correct")
-            elif programConversionOutput != student_numeric_response:
-                print("Incorrect")
             
+            if programConversionOutput == student_numeric_response:
+                print("correct")
+            elif programConversionOutput != student_numeric_response:
+                print("incorrect")
+        else:
+            print("invalid")
+    else:
+        print("invalid")
+
+# Validate the input data values for input_numerical_value and student_numerica_response: The input values entered must be numbers.
+def validateDataTypes(input_numerical_value, student_numeric_response):
+    try:
+        input_numerical_value = float(input_numerical_value)
+    except ValueError:
+        print("input_numerical_value provided is not a number. It's a string")
+        sys.exit('incorrect')
+    
+    try:
+        student_numeric_response = float(student_numeric_response)
+    except ValueError:
+        print("student_numeric_response provided is not a number. It's a string")
+        sys.exit('incorrect')
+
+
 
 input_numerical_value = input("Please enter input_numerical_value: ")
 input_uom = input("Please enter input_uom: ")
 target_uom = input("Please enter target_uom: ")
 student_numeric_response = input("Please enter student_numeric_response: ")
 
-inputs(input_numerical_value, input_uom, target_uom, student_numeric_response)
+
+validateDataTypes(input_numerical_value, student_numeric_response)
+processInputs(input_numerical_value, input_uom, target_uom, student_numeric_response)
 
